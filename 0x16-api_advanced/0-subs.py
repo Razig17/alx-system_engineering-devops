52afa1521cf721e0958c5ca7dbeae6c48e222c83
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-19 05:02:09 +0300

    Return 0 if the subreddit is invalid

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     res = requests.get(url, allow_redirects="false")
-    subs = res.json().get('data').get('subscribers')
+    subs = res.json().get('data')
     if subs:
-        return subs
+        return subs.get('subscribers')
     return 0
