652dd4c5543c3f14ac24b9201a4837d24c18ee3e
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-25 20:19:10 +0300

    Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Update ulimit value
+  exec { 'update_ulimit':
+    command => "sed -i 's/15/4096/' /etc/default/nginx",
+    require => File['/etc/default/nginx']
+  }
+
+# Restart nginx service
+  exec { 'restart_nginx':
+    command     => "service restart nginx",
+  }
