73c0d2258a54e679c46eb707b5b3bc798b7fdc2f
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-25 20:39:25 +0300

    Remove require

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
 # Update ulimit value
   exec { 'update_ulimit':
-    command => "/bin/sed -i 's/15/4096/' /etc/default/nginx",
-    require => File['/etc/default/nginx']
+    command => "/bin/sed -i 's/15/4096/' /etc/default/nginx"
   }
 
 # Restart nginx service
