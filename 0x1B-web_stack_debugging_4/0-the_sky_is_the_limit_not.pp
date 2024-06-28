687ecc8c1020e1308c67bee30d462831b03c893e
Razig17 <abdelrazig.sharif1@gmail.com>
2024-06-28 05:16:00 +0300

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,13 @@
 # Update ulimit value
-  exec { 'update_ulimit':
-    command => "/bin/sed -i 's/15/4096/' /etc/default/nginx"
-  }
-
-# Restart nginx service
-  exec { 'restart_nginx':
-    command  => "/etc/init.d/nginx restart"
-  }
+file { '/etc/default/nginx':
+  ensure  => file,
+  content => '# Note: You may want to look at the following page before setting the ULIMIT.
+#  http://wiki.nginx.org/CoreModule#worker_rlimit_nofile
+# Set the ulimit variable if you need defaults to change.
+#  Example: ULIMIT="-n 4096"
+ULIMIT="-n 4096"'
+}
+-> exec {'Restart the service':
+  command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
